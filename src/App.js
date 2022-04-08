df15247ea88f5bc834529c2f93809bcc85fd73e7
sumon092 <m.sumon@outlook.com>
2022-04-09 02:15:19 +0600

    validation added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const auth = getAuth(app);
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
   }
@@ -19,43 +20,60 @@ function App() {
   }
 
   const handleFormSubmit = (event) => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch(error => {
-        console.error(error)
-      })
-    event.preventDefault();
-  }
-  return (
-    <div>
-      <div className="register mt-2 w-50 mx-auto">
-        <h2 className="text-primary">Please Register</h2>
-        <Form onSubmit={handleFormSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      return;
+    }
+
+    setValidated(true);
+  };
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  event.preventDefault();
+}
+return (
+  <div>
+    <div className="register mt-2 w-50 mx-auto">
+      <h2 className="text-primary">Please Register</h2>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid password.
+        </Form.Control.Feedback>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
