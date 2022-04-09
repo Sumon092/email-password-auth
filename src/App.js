188aae48da4515334dd686af46f42139a2f2fb52
sumon092 <m.sumon@outlook.com>
2022-04-09 13:40:11 +0600

    reg ex added to validation

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [validated, setValidated] = useState(false);
+  const [error, setError] = useState(true)
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
   }
@@ -27,53 +28,60 @@ function App() {
       event.stopPropagation();
       return;
     }
+    if (!/(?=.*[#?!@$%^&*-])/.test(password)) {
+      setError('Password should contain at least one special character');
+      return //error thakle return
+    }
 
     setValidated(true);
-  };
+    setError('');//error na thakle 
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(result => {
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error => {
-      console.error(error)
-    })
-  event.preventDefault();
-}
-return (
-  <div>
-    <div className="register mt-2 w-50 mx-auto">
-      <h2 className="text-primary">Please Register</h2>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid email.
-          </Form.Control.Feedback>
-        </Form.Group>
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    event.preventDefault();
+  }
+  return (
+    <div>
+      <div className="register mt-2 w-50 mx-auto">
+        <h2 className="text-primary">Please Register</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Form.Control.Feedback type="invalid">
-          Please provide a valid password.
-        </Form.Control.Feedback>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+
+          <p className='text-danger'>{error}</p>
+          {/* error message */}
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
